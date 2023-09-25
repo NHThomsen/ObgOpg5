a177bd7dbc5133d6c2c117417d84140e02fe095d
nhthomsen <nooblas@hotmail.com>
2023-09-25 14:10:18 +0200

    Completed assignment

--- TCPClientJson/TCPClientJson/TCPClientJson.py
+++ TCPClientJson/TCPClientJson/TCPClientJson.py
@@ -1,6 +1,27 @@
 from socket import *
+import json
 
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket  = socket(AF_INET,SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
+
+while True:
+    print('Pick Either: Random, Add or Subtract:')
+    case = input()
+    
+    print('Write first number:')
+    firstNumber = input()
+    
+    print('Write second number')
+    secondNumber = input()
+    
+    protocolOptions = {
+        "case" : case,
+        "firstNumber" : int(firstNumber),
+        "secondNumber" : int(secondNumber)
+        }
+    jsonText = json.dumps(protocolOptions)
+    clientSocket.send(jsonText.encode())
+    recievedMessage = clientSocket.recv(1024).decode()
+    print(json.loads(recievedMessage))
--- TCPServerJson/TCPServerJson/TCPServerJson.py
+++ TCPServerJson/TCPServerJson/TCPServerJson.py
@@ -1,8 +1,31 @@
 from socket import *
 import threading
+import json
+import random
 
 def handleClient(connectionSocket, addr):
-    print('hello world')
+    while True:
+        inputRecieved = connectionSocket.recv(1024).decode()
+        jsonText = json.loads(inputRecieved)
+        case = jsonText["case"]
+        firstNumber = int(jsonText["firstNumber"])
+        secondNumber = int(jsonText["secondNumber"])
+        sendResult = ""
+        if case != "Random" and case != "Add" and case != "Subtract":
+            sendResult = case + " unknown case"
+        if case == "Random":
+            if firstNumber > secondNumber:
+                sendResult = "First number is greater than second number"
+            elif firstNumber == secondNumber:
+                sendResult = "First and second number is equal"
+            elif secondNumber > firstNumber:
+                sendResult = json.dumps(random.randint(firstNumber,secondNumber))
+        if case == "Add":
+            sendResult = firstNumber + secondNumber
+        if case == "Subtract":
+            sendResult = firstNumber - secondNumber
+        jsonResult = json.dumps(sendResult)
+        connectionSocket.send(jsonResult.encode())
 
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
